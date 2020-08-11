1b1205317852ec38d6c45a60c76c6c0901e9ffd3
DenisZaychikov <38691003+DenisZaychikov@users.noreply.github.com>
2020-08-11 16:52:39 +0300

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name='Имя')
-    image = models.ImageField(null=True, blank=True, verbose_name='Картинка')
+    image = models.ImageField(verbose_name='Картинка')
     title_en = models.CharField(max_length=200, default='', blank=True, verbose_name='Имя на английском')
     title_jp = models.CharField(max_length=200, default='', blank=True, verbose_name='Имя на японском')
     description = models.TextField(default='', blank=True, verbose_name='Описание')
